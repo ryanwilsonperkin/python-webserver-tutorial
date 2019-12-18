3d65463a66e66060bbfb4d1ad9d93d560a987d09
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2019-12-18 10:38:41 -0500

    Refactor, replacing comments with functions

--- web_server.py
+++ web_server.py
@@ -5,30 +5,29 @@ PORT = 8000
 MAX_REQUEST_BYTES = 1024
 
 
-# Open a new socket on this computer
-with socket.socket() as s:
-
-    # Configure the socket to listen for connections on HOST:PORT
+def configure_socket(s):
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((HOST, PORT))
     s.listen(1)
     print(f"Now listening for connections on http://{HOST}:{PORT}")
 
-    # Process requests until we shut down the server
-    while True:
 
-        # Accept a new connection on this socket so we can read a request
+def load_request(conn):
+    request_bytes = conn.recv(MAX_REQUEST_BYTES)
+    return request_bytes.decode('utf-8')
+
+
+def send_response(conn, response):
+    response_bytes = response.encode('utf-8')
+    conn.sendall(response_bytes)
+
+
+with socket.socket() as s:
+    configure_socket(s)
+    while True:
         conn, addr = s.accept()
         with conn:
-
-            # Read a request from the connection
-            request_bytes = conn.recv(MAX_REQUEST_BYTES)
-            request = request_bytes.decode('utf-8')
-
-            # Print out the request
+            request = load_request(conn)
             print(request)
-
-            # Write a response back on the same connection
-            response = "Hello world"
-            response_bytes = response.encode('utf-8')
-            conn.sendall(response_bytes)
+            response = "Hello World"
+            send_response(conn, response)
