a167ccfcafbe6a9533af92e74bd8cc7ec827dc51
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2019-12-18 16:57:23 -0500

    Add auth middleware

--- web_application.py
+++ web_application.py
@@ -1,14 +1,73 @@
+import json
+
+
 def application(environ, start_response):
-    path = environ['PATH_INFO']
-    handler = get_handler_for_path(path)
-    response = handler(environ)
+
+    response = None
+
+    # Apply middleware
+    middlewares = [AuthMiddleware]
+    for middleware in middlewares:
+        response = middleware().process_request(environ)
+
+        if response is not None:
+            break
+
+    if response is None:
+        path = environ['PATH_INFO']
+        handler = get_handler_for_path(path)
+        response = handler(environ)
+
+    status_code = None
+    for middleware in reversed(middlewares):
+        status_code = middleware().process_response(response)
+
+        if status_code is not None:
+            break
+
+    status_code = status_code or 200
+
     response_bytes = response.encode('utf-8')
 
-    start_response('200 OK', [
+    start_response(str(status_code), [
         ('Content-Length', str(len(response))),
-        ('Content-Type', 'text/html'),
+        ('Content-Type', 'application/json'),
     ])
     return [response_bytes]
+
+
+def parse_cookies(cookies_string):
+    cookies = {}
+
+    if cookies_string == '':
+        return cookies
+
+    cookie_parts = cookies_string.split('; ')
+    for cookie_part in cookie_parts:
+        k, v = cookie_part.split('=')
+        cookies[k] = v
+    return cookies
+
+
+class AuthMiddleware:
+    ALLOWED_USERS = (
+        "Natasha",
+        "Dale",
+        "Ryan"
+    )
+
+    def process_request(self, request):
+        raw_cookie_dough = request.get('HTTP_COOKIE', '')
+        cookies = parse_cookies(raw_cookie_dough)
+        auth = cookies.get('auth')
+        if auth not in self.ALLOWED_USERS:
+            return "Not Allowed"
+        return None
+
+    def process_response(self, response):
+        if response == "Not Allowed":
+            return 403
+        return None
 
 
 def get_handler_for_path(path):
